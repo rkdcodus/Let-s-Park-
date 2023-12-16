89583b80341bb7b07705f399d06dd29d7af7e9fa
kyejin0412@kookmin.ac.kr <kyejin0412@kookmin.ac.kr>
2023-12-17 01:55:18 +0900

    fix: 머지 중 installed_apps에 충돌난 부분 수정

--- LetsPark/LetsPark/settings.py
+++ LetsPark/LetsPark/settings.py
@@ -38,11 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'booking',
-=======
     'accounts',
->>>>>>> yejin
 ]
 
 MIDDLEWARE = [
