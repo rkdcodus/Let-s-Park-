5854412a55d6d86dab9462b2df236d5630ee6b28
kyejin0412@kookmin.ac.kr <kyejin0412@kookmin.ac.kr>
2023-12-17 00:13:01 +0900

    feat: 로그인, 회원가입, 로그아웃 구현 중

--- LetsPark/accounts/models.py
+++ LetsPark/accounts/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Member(models.Model):
-    name = models.CharField(max_length=10)
-    phone_number = models.CharField(max_length=30)
-    car_number = models.CharField(max_length=10)
-    user_id = models.CharField(max_length=20)
+class User(AbstractUser):
+    user_name = models.CharField(max_length=128)
+    user_id = models.CharField(max_length=10)
+    password = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=128)
+    car_number = models.CharField(max_length=8)
     is_booked = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)    
+    is_admin = models.BooleanField(default=False)
--- LetsPark/accounts/views.py
+++ LetsPark/accounts/views.py
@@ -1,28 +1,51 @@
 from django.contrib import auth
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
+User = get_user_model()
+
 def signup(request):
     if request.method == 'POST':
+        if User.objects.filter(user_id=request.POST['user_id']).exists():
+            context['message'] = "아이디가 중복됩니다."
+            return render(request, 'signup.html', context)
         if request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(
-                                            username=request.POST['username'],
+                                            user_id=request.POST['user_id'], 
                                             password=request.POST['password1'],
-                                            email=request.POST['email'],)
+                                            username=request.POST['username'],
+                                            phone_number=request.POST['phone_number'],
+                                            car_number=request.POST['car_number'],
+                                            )
+            user.save();
             auth.login(request, user)
+            
             return redirect('/')
-        return render(request, 'signup.html')
     return render(request, 'signup.html')
+
+# def activate(request, uidb64, token):
+#     try:
+#         uid = force_str(urlsafe_b64decode(uidb64))
+#         user = User.objects.get(pk=uid)
+#     except(TypeError, ValueError, OverflowError, User.DoesNotExsit):
+#         user = None
+#     if user is not None and account_activation_token.check_token(user, token):
+#         user.is_active = True
+#         user.save()
+#         auth.login(request, user)
+#         return redirect('/aaa')
+#     else:
+#         return render(request, 'login.html', {'error' : '계정 활성화 오류'})
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
         user = authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('board')
+            return redirect(request, '/')
         else:
             return render(request, 'login.html', {'error': 'username or password is incorrect.'})
     else:
@@ -30,7 +53,7 @@ def login(request):
 
 def logout(request):
     auth.logout(request)
-    return redirect('')
+    return redirect('/')
 
 def home(request):
     return render(request, 'accounts_home.html')
