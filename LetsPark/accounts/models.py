a38ee7b402a00de3a4fd8e29ed90c4899abed82b
rkdcodus <rcd7275@naver.com>
2023-12-17 16:17:09 +0900

    User 모델 변경

--- LetsPark/accounts/models.py
+++ LetsPark/accounts/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    user_name = models.CharField(max_length=128)
-    user_id = models.CharField(max_length=10)
     password = models.CharField(max_length=128)
     phone_number = models.CharField(max_length=128)
     car_number = models.CharField(max_length=8)
