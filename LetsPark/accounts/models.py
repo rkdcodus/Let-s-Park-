c7398ecaa6c29645d132c767c36c213b70871dd3
rkdcodus <rcd7275@naver.com>
2023-12-17 17:29:04 +0900

    User 모델 park 외래키 추가

--- LetsPark/accounts/models.py
+++ LetsPark/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from booking.models import Park
 
 class User(AbstractUser):
     password = models.CharField(max_length=128)
@@ -8,3 +9,4 @@ class User(AbstractUser):
     is_booked = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)    
     is_admin = models.BooleanField(default=False)
+    park = models.ForeignKey(Park, on_delete=models.CASCADE, null=True, blank=True)
